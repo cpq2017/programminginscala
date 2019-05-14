bd20d3dc97a7fa6baf654e51b5fc5d31259077f3
cpq2017 <cpq2014@163.com>
2019-05-15 00:33:06 +0800

    scala 编程第三版学习 1

--- src/main/scala/com/cpq/TestDemo.scala
+++ src/main/scala/com/cpq/TestDemo.scala
@@ -1,13 +1,17 @@
 package com.cpq
-
+//import org.scalatest._
 /**
   * Created by Enzo Cotter on 2019/5/14.
   */
-object Test extends  App {
+object TestDemo extends  App {
   var capital = Map("US"->"Washington","France" -> "Paris")
   capital += ("Chain"->"Beijing")
   println(capital("US"))
   def factorial(s:BigInt):BigInt =
     if(s ==0 ) 1 else s* factorial(s-1)
   println(factorial(30))
+  val xs = 1 to 3
+  val it = xs.iterator
+
+//  eventually {it.next() shouldBe 3}
 }
